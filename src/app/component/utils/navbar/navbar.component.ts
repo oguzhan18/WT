322621f0d178fc36b0665d263c655aed4e543716
oguzhan18 <oguzhancart1@gmail.com>
2022-08-03 14:10:52 +0300

    The last operations are almost complete. job done dad

--- src/app/component/utils/navbar/navbar.component.ts
+++ src/app/component/utils/navbar/navbar.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  isCollapsed = false;
 
+  toggleNavbar() {
+    this.isCollapsed = !this.isCollapsed;
+  }
   constructor() { }
 
   ngOnInit(): void {
